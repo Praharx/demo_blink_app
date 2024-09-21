c89979ae6d2f65561140a2b1bdb4577714c4d8d8
krishhh16 <krishh16@yahoo.com>
2024-09-21 20:19:26 +0530

    added frontend components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import Navbar from "@/components/App/Navbar";
+import { FeaturesSectionDemo } from "@/components/App/Features";
 import VortexBackground from "@/components/App/Vortex";
-
+import { FloatingNav } from "@/components/ui/floating-navbar";
 export default function Home() {
   return (
-   <div>
-      <Navbar />
+   <div className=" w-screen">
+      <FloatingNav navItems={[{name: 'Home', link: '/'}, {name: 'About', link: '/about'}, {name: 'Contact', link: '/contact'}]} />
       <VortexBackground />
+      <FeaturesSectionDemo/>
    </div>
   );
 }
--- components/App/Vortex.tsx
+++ components/App/Vortex.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react";
 import { Vortex } from "../ui/vortex";
 
@@ -7,16 +9,16 @@ export default function VortexBackground() {
       <Vortex
         backgroundColor="black"
         rangeY={800}
-        particleCount={500}
-        baseHue={120}
+        particleCount={100}
+        baseHue={10}
         className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full"
       >
         <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
-          The hell is this?
+          Sell through Solana Blinks
         </h2>
         <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
-          This is chemical burn. It&apos;ll hurt more than you&apos;ve ever been
-          burned and you&apos;ll have a scar.
+          Take sale & distribution to a next-level scale with
+            {<span className="font-bold">blinkify</span>} - sell your products through blinks.
         </p>
         <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
           <button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
