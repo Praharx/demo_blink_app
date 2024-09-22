721ebcc8b1d186e32bccd05b8ef3f3ad24082a90
krishhh16 <krishh16@yahoo.com>
2024-09-22 18:25:44 +0530

    added conditional rendering for the navbar buttons for correct redirection of the user

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { SignInButton,SignUpButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { SignInButton,SignOutButton,SignUpButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import React, { useState } from "react";
 import {
   motion,
@@ -13,6 +13,8 @@ import { useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { useWalletModal } from "@solana/wallet-adapter-react-ui";
 import { Button } from "./button";
+import {useUser} from "@clerk/nextjs";
+
 export const FloatingNav = ({
   navItems,
   className,
@@ -29,6 +31,7 @@ export const FloatingNav = ({
   const { scrollYProgress } = useScroll();
   const [visible, setDropDownVisible] = useState(true);
   const [dropdownVisible, setDropdownVisible] = useState(false); // New state for dropdown visibility
+  const {isSignedIn, isLoaded, user} = useUser();
 
   useMotionValueEvent(scrollYProgress, "change", (current) => {
     if (typeof current === "number") {
@@ -51,55 +54,65 @@ export const FloatingNav = ({
           duration: 1,
         }}
         className={cn(
-          "flex max-w-fit fixed top-0 inset-x-0 mx-auto border border-white/[0.2] rounded-full bg-black shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2 items-center justify-center space-x-4",
+          "flex max-w-4xl justify-between  fixed top-0 inset-x-0 mx-auto border border-white/[0.2] rounded-full bg-black shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2 items-center  space-x-4",
           className
         )}
       >
-        <Logo/>
-        {navItems.map((navItem: any, idx: number) => (
-          <Link
-            key={`link=${idx}`}
-            href={navItem.link}
-            className={cn(
-              "relative text-neutral-50 items-center flex space-x-1 hover:text-neutral-300"
-            )}
-          >
-            <span className="block sm:hidden">{navItem.icon}</span>
-            <span className="hidden sm:block text-sm">{navItem.name}</span>
-          </Link>
-        ))}
-        {publicKey ? (
-          <div className="relative">
-            <Button
-              className="border text-sm font-medium relative border-neutral-200 border-white/[0.2] text-white px-4 py-2 rounded-full"
-              onClick={() => setDropdownVisible(!dropdownVisible)} // Toggle dropdown visibility
-            >
-              <span>{`${publicKey.toString().slice(0, 4)}...${publicKey.toString().slice(-4)}`}</span>
-              <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent h-px" />
-            </Button>
-            {dropdownVisible && ( // Conditionally render the dropdown
-              <div className="absolute right-0 mt-2 w-48 bg-black border border-white/[0.2] rounded-md shadow-lg z-50">
-                <Button onClick={() => disconnect()} className="w-full text-left px-4 py-2 text-sm text-white hover:bg-neutral-700">Disconnect Wallet</Button>
-                <Button onClick={() => setVisible(true)} className="w-full text-left px-4 py-2 text-sm text-white hover:bg-neutral-700">Change Wallet</Button>
-                <Button onClick={() => navigator.clipboard.writeText(publicKey.toString())} className="w-full text-left px-4 py-2 text-sm text-white hover:bg-neutral-700">Copy Address</Button>
+        <Logo/>   
+        <div className="flex items-center space-x-4">
+            {publicKey && isSignedIn ? (
+              <div className="relative">
+                <Button
+                  className="border text-sm font-medium relative border-neutral-200 border-white/[0.2] text-white px-4 py-2 rounded-full"
+                  onClick={() => setDropdownVisible(!dropdownVisible)}
+                >
+                  <span>{`${publicKey.toString().slice(0, 4)}...${publicKey.toString().slice(-4)}`}</span>
+                  <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent h-px" />
+                </Button>
+                {dropdownVisible && (
+                  <div className="absolute right-0 mt-2 w-48 bg-black border border-white/[0.2] rounded-md shadow-lg z-50">
+                    <Button onClick={() => disconnect()} className="w-full text-left px-4 py-2 text-sm text-white hover:bg-neutral-700">Disconnect Wallet</Button>
+                    <Button onClick={() => setVisible(true)} className="w-full text-left px-4 py-2 text-sm text-white hover:bg-neutral-700">Change Wallet</Button>
+                    <Button onClick={() => navigator.clipboard.writeText(publicKey.toString())} className="w-full text-left px-4 py-2 text-sm text-white hover:bg-neutral-700">Copy Address</Button>
+                  </div>
+                )}
               </div>
+            ) : 
+              <Button onClick={() => setVisible(true)} className="border text-sm font-medium relative border-neutral-200 border-white/[0.2] text-white px-4 py-2 rounded-full">
+                <span>Select Wallet</span>
+                <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent h-px" />
+              </Button>
+            }
+            {!isSignedIn ? (
+              <SignInButton>
+                <Button className="border text-sm font-medium relative border-neutral-200 border-white/[0.2] text-white px-4 py-2 rounded-full">
+                  <span>Login</span>
+                  <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
+                </Button>
+              </SignInButton>
+            ) : (
+              <Link href="/dashboard">
+                <Button className="border text-sm font-medium relative border-neutral-200 border-white/[0.2] text-white px-4 py-2 rounded-full">
+                  <span>Dashboard</span>
+                  <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
+                </Button>
+              </Link>
             )}
-          </div>
-        ) : (
-          <WalletMultiButton style={{ border: '1px solid rgba(255, 255, 255, 0.2)', backgroundColor: 'white',color: 'black', padding: '8px 16px', borderRadius: '9999px', height: '32px' }} />
-        )}
-        <SignInButton>
-        <Button className="border text-sm font-medium relative border-neutral-200 border-white/[0.2] text-white px-4 py-2 rounded-full">
-          <span>Login</span>
-          <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
-        </Button>
-        </SignInButton>
-        <SignUpButton>
-        <Button className="border text-sm font-medium relative border-neutral-200 border-white/[0.2] text-white px-4 py-2 rounded-full">
-          <span>SignUp</span>
-          <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
-        </Button>
-        </SignUpButton>
+            {!isSignedIn ?<SignUpButton>
+            <Button className="border text-sm font-medium relative border-neutral-200 border-white/[0.2] text-white px-4 py-2 rounded-full">
+              <span>SignUp</span>
+              <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
+            </Button>
+            </SignUpButton>
+            :
+              <SignOutButton>
+                <Button className="border text-sm font-medium relative border-neutral-200 border-white/[0.2] text-white px-4 py-2 rounded-full">
+                  <span>Log out</span>
+                  <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
+                </Button>
+              </SignOutButton>
+          }
+        </div>
       </motion.div>
     </AnimatePresence>
   );
